fb27ce0fb470c373834c527d9a0568869888fac1
Zhuzhgin <artemfox@list.ru>
2022-01-27 01:11:59 +0500

    End

--- CoreDataProject/AppDelegate.swift
+++ CoreDataProject/AppDelegate.swift
@@ -6,24 +6,19 @@
 //
 
 import UIKit
-//import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
     
-
+    var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-            window?.rootViewController = UINavigationController(rootViewController: TaskListViewController())
-
+        window?.rootViewController = UINavigationController(rootViewController: TaskListViewController())
+        
         return true
     }
-
-
 }
 
